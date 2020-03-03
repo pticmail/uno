e02bb8ce4d51b5232881034e0b0745209ae03ba0
pablo <pablo@MSI>
2020-03-03 11:15:03 +0100

    inicio

---
+++ src/org/pablo/mylib/LibClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pablo.mylib;
+
+/**
+ *
+ * @author pablo
+ */
+public class LibClass {
+
+    public static String acrostic(String[] args) {
+        StringBuilder b = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].length() > i) {
+                b.append(args[i].charAt(i));
+            } else {
+                b.append('?');
+            }
+        }
+        return b.toString();
+    }
+}
---
+++ test/org/pablo/mylib/LibClassTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pablo.mylib;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author pablo
+ */
+public class LibClassTest {
+    
+    public LibClassTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of acrostic method, of class LibClass.
+     */
+    @Test
+    public void testAcrostic() {
+        System.err.println("Running testAcrostic...");
+        String result = LibClass.acrostic(new String[]{"fnord", "polly", "tropism"});
+        assertEquals("Correct value", "foo", result);
+    }
+    
+}
